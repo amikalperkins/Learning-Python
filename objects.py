1dcad493c21907a8481f804e9d562554a968184f
amikalperkins <a.mikalperkins@gmail.com>
2021-11-03 14:05:24 -0700

    all things are objects in python

---
+++ objects.py
@@ -0,0 +1,14 @@
+#type function displays object type
+
+print(type("hello, world!")); #string
+print(type(17));
+print(type(42000));
+
+# type conversion
+print(3.13, int(3.14));
+
+
+# Names assigned to objects are variables
+
+firstName = "Brad";
+print(firstName);
