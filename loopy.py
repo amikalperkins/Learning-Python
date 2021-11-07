c775afa88fa38a79e93bad8bf923caff61a081da
amikalperkins <a.mikalperkins@gmail.com>
2021-11-07 05:41:49 -0800

    update

---
+++ loopy.py
@@ -0,0 +1,14 @@
+# for loop_var [sequence] : 
+#   indented statement block
+#   python uses indentions instead of curly braces
+
+import turtle
+
+ws = turtle.Screen()
+mikey = turtle.Turtle()
+
+for iterator in [0,1,2,3]:
+    mikey.forward(50)
+    mikey.left(90)
+
+
