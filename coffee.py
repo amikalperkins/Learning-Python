4988105609a7cb28d9efe5975383a21de7343238
amikalperkins <a.mikalperkins@gmail.com>
2021-11-07 11:44:31 -0800

    Coffee partay

--- coffee.py
+++ coffee.py
@@ -3,13 +3,24 @@ import coffee_shop
 print("Welcome to", coffee_shop.shop_name)
 print("Available sizes:", coffee_shop.coffee_sizes)
 print("Available roasts:", coffee_shop.coffee_roasts)
+print()
 
 # Get some input from user
 order_size = input("What size coffee would you like? ")
 order_roast = input("What roast would you like? ")
 
+
 # Send the order to the coffee shop module
 shop_says = coffee_shop.order_coffee(order_size, order_roast)
-
 # Print out what it gives back to us
 print(shop_says)
+print()
+
+# See if customer wants to add milk
+add_milk_response = input("Would you like to add milk (y/n)? ")
+# Convert the response to lowercase, then check for a "yes" answer
+if "y" in add_milk_response.lower():
+    milk_fat = input("What percent milk do you want added? ")
+    shop_says = coffee_shop.add_milk_please(milk_fat)
+    # Print what it gave back to us
+    print(shop_says)
--- coffee_shop.py
+++ coffee_shop.py
@@ -14,4 +14,23 @@ def order_coffee(size, roast):
     : parameter roast: a string containing one of the coffee roasts
     : return: a message about the coffee order
     """
-    return "Here's your {} coffee roasted {}".format(size, roast)
+    return "Here's your {} coffee roasted {}.".format(size, roast)
+
+def add_milk_please(fat_content):
+    """
+    Give an option to add milk
+    : parameter fat_content : a string or integer containing the milkfat content
+    : return : a message about having added milk
+    """
+    return "We have added {}% milk you requested.".format(fat_content)
+
+def give_tip(tip_amount):
+    """
+    Take a tip from the user, then be happy about it
+    : parameter tip_amount : the tip amount
+    : return : nothing
+    """
+    print("Thank you so much! We don't make a ton of money.")
+    
+    # Not having a return statement defaults to None
+
