1b0b341daf9ebd53410c171c5a186d1e0de9a203
amikalperkins <a.mikalperkins@gmail.com>
2021-12-20 11:53:56 -0800

    update

--- 18GetStartData.py
+++ 18GetStartData.py
@@ -1,5 +1,23 @@
-float_radius = float(input("Please enter the radius of a circle: "))
-print(type(float_radius))
+# float_radius = float(input("Please enter the radius of a circle: "))
+# print(type(float_radius))
 
-diameter = 2 * float_radius
-print(type(diameter))
+# diameter = 2 * float_radius
+# print(type(diameter))
+
+wordlist = ['cat', 
+            'dog', 
+            'rabbit']
+# outer loop is the list of words 0 cat, 1 dog, 2 rabbit
+# inner loop iterates over the characters 0 [0,1,2] 
+#                                         1 [0,1,2] 
+#                                         2 [0,1,2,3,4,5]
+letterlist = set([aletter for aword in wordlist for aletter in aword])
+
+# sets do not allow duplicates
+# and then convert back to a list
+# for aword in wordlist:
+#     for aletter in aword:
+#         if aletter not in letterlist:
+#             letterlist.append(aletter)
+
+print((list(letterlist)))
